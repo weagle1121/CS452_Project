ec8e4efd2704dd45d164b2d19d3e09618f1291aa
weagle1121 <weagle1121@gmail.com>
2016-09-04 22:30:44 -0500

    Add files via upload
    lastest version of main and header files

---
+++ Nucor_Header.php
@@ -0,0 +1,34 @@
+<!-- header file -->
+<div class = "header">
+<div id ="banner">
+	<img class="banner1" src="Banner.jpg" alt="logo">
+</div>
+
+    <div id="topHeaderRow" >
+        <nav class ="navbar navbar-inverse navbar-static">
+	        
+	        <div class = "container-fluid">
+<!-- 		        Logo -->
+				<div class = "navbar-header">
+<!-- 					Mobile Button-->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNavBar"> 
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</button>
+					<a href="#" class = "navbar-brand">NUCOR RELAY FOR LIFE</a>
+				</div>
+				<!-- Menu Items -->
+	        <div class ="collapse navbar-collapse" id ="mainNavBar">
+		        <ul class = "nav navbar-nav navbar-right">
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#"> RELAY TEAMS</a></li>
+                <li><a href="#">LEADER BOARD</a></li>
+                <li><a href="#">EVENTS</a></li>  
+                <li><a href="#">FORUM</a></li> 
+                <li><a href="#">PHOTO GALLERY</a></li>                  
+            </ul>
+
+	        </div>
+	        </div>
+        </nav>
---
+++ Nucor_Main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--Shannon LaMar   
+  9/2/1016
+  Nucor RFL Main-->
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Nucor Relay for Life</title>
+   <!-- Bootstrap core CSS -->
+    <link href="Bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this page -->
+    <link href="Nucor_Main_Page.css" rel="stylesheet">
+<?php include 'Nucor_Header.php';?>
+</head>
+
+<body>
+<!-- 	Bootstrap core JavaScript -->
+    <script src="Bootstrap/bootstrap-3.0.0/assets/js/jquery.js"></script>
+    <script src="Bootstrap/bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>    
+	
+</body>
+</html>
