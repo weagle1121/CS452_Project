26b1fca59f7f5588248394174cbde82d0eb5384f
weagle1121 <weagle1121@gmail.com>
2016-09-25 15:48:27 -0500

    Add files via upload
    Updated with dummy lists

---
+++ Nucor_Leader_Board.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--Shannon LaMar   
+  9/2/1016
+  Nucor RFL LEADER BOARD-->
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Nucor Relay for Life</title>
+   <!-- Bootstrap core CSS -->
+    <link href="Bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this page -->
+    <link href="Nucor_Main_Page.css" rel="stylesheet">
+<?php include 'Nucor_Header.php';?>
+</head>
+
+<body>
+	
+	
+<div class="container">
+  <h2>Decatur Relay Teams</h2>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Rank</th>
+        <th>Team Name</th>
+        <th>Money Raised</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Angry Armadillos</td>
+        <td>1111</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Crazy Cats</td>
+        <td>222</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Bashful Badgers</td>
+        <td>33</td>
+      </tr>
+    </tbody>
+  </table>
+</div>	
+	
+<!-- 	Bootstrap core JavaScript -->
+    <script src="Bootstrap/bootstrap-3.0.0/assets/js/jquery.js"></script>
+    <script src="Bootstrap/bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>    
+	
+</body>
+</html>
---
+++ Nucor_Relay_Teams.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--Shannon LaMar   
+  9/2/1016
+  Nucor RFL RELAY TEAMS->
+<html>
+<head lang="en">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Nucor Relay for Life</title>
+   <!-- Bootstrap core CSS -->
+    <link href="Bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this page -->
+    <link href="Nucor_Main_Page.css" rel="stylesheet">
+<!--     header file -->
+<?php include 'Nucor_Header.php';?>
+</head>
+
+<body>
+<div class="container">
+  <h2>Decatur Relay Teams</h2>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th>Team Name</th>
+        <th>Team Captain</th>
+        <th>Phone Number</th>
+        <th>Email Address</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Angry Armadillos</td>
+        <td>Andy Anderson</td>
+        <td>111-1111</td>
+        <td>andy@example.com</td>
+      </tr>
+      <tr>
+        <td>Bashful Badgers</td>
+        <td>Betty Batson</td>
+        <td>222-2222</td>
+        <td>betty@example.com</td>
+      </tr>
+      <tr>
+        <td>Crazy Cats</td>
+        <td>Casey Clark</td>
+        <td>333-3333</td>
+        <td>casey@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>	
+
+	
+<!-- 	Bootstrap core JavaScript -->
+    <script src="Bootstrap/bootstrap-3.0.0/assets/js/jquery.js"></script>
+    <script src="Bootstrap/bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>    
+	
+</body>
+</html>
