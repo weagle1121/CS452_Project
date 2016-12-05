1e1254ad9e03e2f870c2233ddc0d8eb527d9f18d
shaundyer <shaun.dyer@gmail.com>
2016-12-05 11:21:13 -0600

    Better error handling for DB Connection
    Instead of showing the full text exception when a database connection is
    unavailable the script now shows that the DB Connection is unavailable
    and exits.

--- backend/_db.php
+++ backend/_db.php
@@ -1,4 +1,13 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=nucor;charset=utf8', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dsn = 'mysql:dbname=nucor;host=localhost;charset=utf8';
+$user = 'root';
+$password = '';
+try {
+	$db = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//	$db = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+} catch (PDOException $e) {
+	$dbfailmsg = '<h1>Database connection unavailable.</h1>';
+	echo $dbfailmsg;
+	exit;
+}
 ?>
