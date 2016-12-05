e5da67439b9703a37c17559a19da27d767cabfce
shaundyer <shaun.dyer@gmail.com>
2016-12-05 16:53:29 -0600

    Photos Query fixed

--- photos.php
+++ photos.php
@@ -12,21 +12,20 @@ Adam Moses
 	<title>Photo Gallery</title>
    <!-- Bootstrap core CSS -->
     <link href="Bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
+
 	<link href="drop_menu/drop_menu.css" rel="stylesheet">
 	<!-- Custom styles for this site -->
 	<link href="main_page.css" rel="stylesheet">
 
 
-
-
   <!-- Custom styles for this page -->
   <style type="text/css">
 
-  /*background image, should work across web browsers*/
+  /*background image, should work across web browsers except internet explorer*/
 
   body {
   background-image: URL(pictures/photo_bkgr.png);
-/*   background-repeat: no-repeat; */
+  background-repeat: no-repeat;
   background-position: center center fixed;	
   background-attachment: fixed;
   -webkit-background-size: cover;
@@ -47,14 +46,15 @@ Adam Moses
       <select id="selectedAlbum" onchange="selectAlbum()">
 <?php
 require_once 'backend/_db.php';
-$stmt = $db->prepare("SELECT DISTINCT images.imageALBUM FROM images WHERE imageACTIVE = TRUE");
+$stmt = $db->prepare('SELECT * FROM albums WHERE id IN (SELECT imageALBUM FROM images WHERE imageACTIVE = TRUE) ORDER BY date DESC');
 	$stmt->execute();
 	$result = $stmt->fetchAll();
+	var_dump ($result);
  if($stmt->rowCount())
  {
-	 foreach($result as $row)
+	 foreach($result as $albumRow)
 	 {
-		 echo '        <option value="'.$row["imageALBUM"].'">'.$row["imageALBUM"].'</option>
+		 echo '        <option value="'.$albumRow['id'].'">'.$albumRow['date'].' - '.$albumRow['name'].'</option>
 ';
 	 }
  }
@@ -65,13 +65,16 @@ $stmt = $db->prepare("SELECT DISTINCT images.imageALBUM FROM images WHERE imageA
 </div>
 <div id="slide_show">
 <!--Data populated by JS Function--></div>
+	<script src="Bootstrap/bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>
+	<script src="Bootstrap/bootstrap-3.0.0/assets/js/jquery.js"></script>
+	<script src="Bootstrap/bootstrap-3.0.0/js/carousel.js"></script>
 <script>
   $(document).ready(function()
     {
     $.post("backend/backend_photos.php", 
       {
       // variable_name:data
-      selectedAlbum:"SampleAlbum"
+      selectedAlbum:<?php echo $result[0]['id']; ?>
       },
       function(data)
 	  {
@@ -95,10 +98,5 @@ $stmt = $db->prepare("SELECT DISTINCT images.imageALBUM FROM images WHERE imageA
     }
 </script>
 <!-- 	Bootstrap core JavaScript -->
-
-	<script src="Bootstrap/bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>
-	<script src="Bootstrap/bootstrap-3.0.0/assets/js/jquery.js"></script>
-	<script src="Bootstrap/bootstrap-3.0.0/js/carousel.js"></script>
-
 </body>
 </html>
